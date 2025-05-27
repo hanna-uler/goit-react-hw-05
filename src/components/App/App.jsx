136f270ed9e6e4bb9b39e5d696e42fbb9c6729f0
hanna-uler <hannauler@gmail.com>
2025-05-27 14:15:33 -0400

    add routes and homepage nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,21 @@
 import css from './App.module.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from '../../pages/HomePage/HomePage'
+import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage'
+import MoviesPage from '../../pages/MoviesPage/MoviesPage'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage'
+
 
 export default function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/movies' element={<MoviesPage/>} />
+        <Route path='/movie-details' element={<MovieDetailsPage/>}></Route>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Routes>
+    </div>
   )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,17 @@
 import css from './Navigation.module.css'
+import { NavLink } from 'react-router-dom'
+
 export default function Navigation() {
-    
+    return (
+        <div>
+            <nav className={css.navMenu}>
+                <NavLink to='/' className={css.navLink}>
+                    Home
+                </NavLink>
+                <NavLink to='/movies' className={css.navLink}>
+                Movie Catalogue
+                </NavLink>
+            </nav>
+        </div>
+    )
 }
