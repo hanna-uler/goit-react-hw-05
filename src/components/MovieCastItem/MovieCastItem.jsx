f744310e395a8ac8f9a3c0b1d0f1416e2d5efbfd
hanna-uler <hannauler@gmail.com>
2025-05-28 15:05:49 -0400

    add movie cast element comp

---
+++ src/components/MovieCastItem/MovieCastItem.jsx
@@ -0,0 +1,11 @@
+import css from './MovieCastItem.module.css'
+
+export default function MovieCastItem() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <p>The Movie Character: </p>
+            <p>Actor's name: </p>
+        </div>
+    )
+}
