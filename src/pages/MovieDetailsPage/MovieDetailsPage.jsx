3b7e9797e15a1536abc5776c97575801260b3336
hanna-uler <hannauler@gmail.com>
2025-05-28 14:57:50 -0400

    get movie details, add not found text

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,20 +1,39 @@
-import { useParams,Outlet, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Outlet, Link, NavLink } from "react-router-dom";
+import axios from "axios";
+import css from "./MovieDetailsPage.module.css"
 
 export default function MovieDetailsPage() {
     const { movieId } = useParams();
-    // fetchMovieData ({movieId})
-    return (
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}`;
+        const options = {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjUyODNiYTMxODQzNDY1YzY3NzQyYmMzM2U3Y2RhMyIsIm5iZiI6MTc0ODIzMTUxNC40ODk5OTk4LCJzdWIiOiI2ODMzZTU1YTcwMzE1ZjM0ODEyYjcxMTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._rMCNG9BRzj5XkY_P2maKWqXo77F7leFnuMpJqC45Qs'
+            }
+        };
+        axios.get(url, options).then((res) => setMovie(res.data));
+    }, [movieId])
+    console.log(movie);
+    if (movie) {
+        return (
         <div>
-            <p>The Movie with id {movieId}. Details will display here.</p>
-            <ul>
+            <h1>{movie.original_title}</h1>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`The poster of ${movie.original_title} movie.`} /> 
+            <p>Movie Budget: ${ movie.budget}</p>
+            <h3>The movie overview:</h3>
+            <p>{movie.overview}</p>
+            <ul className={css.navMenu}>
                 <li>
-                    <Link to='cast'>Cast</Link>
+                    <NavLink to='cast'>Cast</NavLink>
                 </li>
                 <li>
-                    <Link to='reviews'>Reviews</Link>
+                    <NavLink to='reviews'>Reviews</NavLink>
                 </li>
             </ul>
             <Outlet />
         </div>
-    )
+    )}
 }
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,6 @@
 import css from './NotFoundPage.module.css'
 export default function NotFoundPage() {
-    
+    return (
+        <strong>Sorry, the page is not found.</strong>
+    )
 }
