64c3f0a3fcfcba1504fb83dc9c8991478c7947f8
hanna-uler <hannauler@gmail.com>
2025-05-28 16:02:28 -0400

    add movie cast item comp

--- src/components/MovieCastItem/MovieCastItem.jsx
+++ src/components/MovieCastItem/MovieCastItem.jsx
@@ -1,11 +1,11 @@
 import css from './MovieCastItem.module.css'
 
-export default function MovieCastItem() {
+export default function MovieCastItem({picPath, character, name}) {
     return (
-        <div>
-            <img src="" alt="" />
-            <p>The Movie Character: </p>
-            <p>Actor's name: </p>
+        <div className={css.container}>
+            <img className={css.actorPic} src={`https://image.tmdb.org/t/p/w500/${picPath}`} alt={`The picture of ${name}.`} />
+            <p className={css.title}>The Movie Character: <span className={css.name}>{character}</span></p>
+            <p className={css.title}>Actor's name: <span className={css.name}>{ name}</span></p>
         </div>
     )
 }
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -9,7 +9,7 @@ export default function MovieList({ movies }) {
                     return (
                         <li key={movie.id} className={css.movieItem} >
                             <div>
-                                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`The poster of {movie.title} movie.`} />
+                                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`The poster of ${movie.title} movie.`} />
                                 <h3 className={css.movieTitle}>{movie.title}</h3>
                                 <p>Release date: {movie.release_date}</p>
                                 <Link className={css.detLink} to={`/movies/${movie.id}`} >Details</Link>
