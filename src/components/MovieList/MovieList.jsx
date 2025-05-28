8587f453db12a1015236fd8919cd8365b956591e
hanna-uler <hannauler@gmail.com>
2025-05-28 13:06:50 -0400

    add details link

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,18 +1,18 @@
+import { Link } from 'react-router-dom'
 import css from './MovieList.module.css'
 
 export default function MovieList({ movies }) {
-    // onClick - link to MovieDetailsPage prop movie id
     return (
         <div className={css.container}>
-            <p>Movies List</p>
             <ul className={css.moviesList}>
                 {movies.map((movie) => {
                     return (
-                        <li key={movie.id} className={css.movieItem} onClick={(e) => console.log("event.target: ", e.target)}>
+                        <li key={movie.id} className={css.movieItem} >
                             <div>
                                 <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={`The poster of {movie.title} movie.`} />
                                 <h3 className={css.movieTitle}>{movie.title}</h3>
                                 <p>Release date: {movie.release_date}</p>
+                                <Link className={css.detLink} to={`/movies/${movie.id}`} >Details</Link>
                             </div>
                         </li>
                     )
@@ -22,5 +22,3 @@ export default function MovieList({ movies }) {
         </div>
     )
 }
-
-// https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,11 @@
 import { useParams,Outlet, Link } from "react-router-dom";
 
 export default function MovieDetailsPage() {
-    const { movieId } = useParams;
+    const { movieId } = useParams();
     // fetchMovieData ({movieId})
     return (
         <div>
-            <p>The Movie with id {movieId} Details will display here.</p>
+            <p>The Movie with id {movieId}. Details will display here.</p>
             <ul>
                 <li>
                     <Link to='cast'>Cast</Link>
