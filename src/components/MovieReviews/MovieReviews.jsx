4cc3f1db28c5057773295d68327d17702371f28e
hanna-uler <hannauler@gmail.com>
2025-05-28 16:45:08 -0400

    add reviews request and markup

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,4 +1,39 @@
 import css from './MovieReviews.module.css'
+import MovieCastItem from '../MovieCastItem/MovieCastItem'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
 export default function MovieReviews() {
+    const { movieId } = useParams();
+    const [reviewsArray, setReviewsArray] = useState([]);
+    
+    useEffect(() => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews`;
+        const options = {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjUyODNiYTMxODQzNDY1YzY3NzQyYmMzM2U3Y2RhMyIsIm5iZiI6MTc0ODIzMTUxNC40ODk5OTk4LCJzdWIiOiI2ODMzZTU1YTcwMzE1ZjM0ODEyYjcxMTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._rMCNG9BRzj5XkY_P2maKWqXo77F7leFnuMpJqC45Qs'
+            }
+        };
+        axios.get(url, options).then((res) => setReviewsArray(res.data.results));
+    }, [movieId])
+    if (reviewsArray.length >0) {
+        
+    return (
+        <div className={css.container}>
+            <h2 className={css.reviewsTitle}>The movie reviews:</h2>
+            <ul className={css.reviewsList}>
+                {reviewsArray.map((review) => {
+                    return (
+                        <li key={review.id} className={css.reviewsItem}>
+                            <p>{ review.author}</p>
+                            {/* <MovieReviewsItem /> */}
+                        </li>  
+                    )
+                })}
+            </ul>
+        </div>
+    )
+    }
     
 }
