a9d1fb053d86b60509a4cbc68ca1012437da0a1c
hanna-uler <hannauler@gmail.com>
2025-05-29 12:02:23 -0400

    move search to MoviePages, add controlled input, use debounce and SearchParams, delete SearchBar

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 import css from './MoviesPage.module.css'
 import axios from 'axios'
 import MovieList from '../../components/MovieList/MovieList'
-import SearchBar from '../../components/SearchBar/SearchBar'
+import { useDebounce } from 'use-debounce';
 
 export default function MoviesPage() {
     const [moviesArray, setMoviesArray] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get("query") ?? "";
+    const [debouncedQuery] = useDebounce(query, 300);
 
-    const fetchPics = (searchQuery) => {
-        const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}`;
+
+    const changeSearchQuery = (e) => {
+        const newQuery = e.target.value;
+        const nextSearchParams = new URLSearchParams(searchParams);
+    
+        if (newQuery !== "") {
+          nextSearchParams.set("query", newQuery);
+        } else {
+          nextSearchParams.delete("query");
+        }
+    
+        setSearchParams(nextSearchParams);
+    };
+    
+    useEffect(() => {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${debouncedQuery}`;
         const options = {
             headers: {
                 accept: 'application/json',
@@ -16,12 +34,12 @@ export default function MoviesPage() {
             }
         };
         axios.get(url, options).then((res) => setMoviesArray(res.data.results));
-    };
+    }, [debouncedQuery])
 
     return (
         <div className={css.container}>
             <h1>Search movie here</h1>
-            <SearchBar onSubmit={fetchPics}/>
+            <input className={css.input} type="text" value={query} onChange={changeSearchQuery} />
             {moviesArray.length > 0 && <MovieList movies={moviesArray} />}
         </div>
     )
