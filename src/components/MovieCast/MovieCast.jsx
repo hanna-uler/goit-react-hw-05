84eba2336c5420593a8dc1376ebc8f451ca7dab8
hanna-uler <hannauler@gmail.com>
2025-05-30 00:20:59 -0400

    add error handeling to cast and reviews comps

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -5,12 +5,11 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
-
 export default function MovieCast() {
     const { movieId } = useParams();
     const [actorsArray, setActorsArray] = useState([]);
     const [hasLoaded, setHasLoaded] = useState(false);
-    // const [isError, setIsError] = useState(false);
+    const [isError, setIsError] = useState(false);
     
     useEffect(() => {
         const token = import.meta.env.VITE_TMDB_TOKEN;
@@ -21,7 +20,7 @@ export default function MovieCast() {
                 Authorization: `Bearer ${token}`,
             }
         };
-        // setIsError(false);
+        setIsError(false);
         setHasLoaded(false);
         axios.get(url, options)
             .then((res) => {
@@ -29,28 +28,27 @@ export default function MovieCast() {
                 setHasLoaded(true);
             })
             .catch(() => {
-                setActorsArray([]);
-                // setIsError(true);
+                setIsError(true);
             });
     }, [movieId]);
-    if (actorsArray.length > 0) {
-        return (
-            <div className={css.container}>
-            <h2 className={css.castTitle}>The movie cast:</h2>
-            <ul className={css.castList}>
-                {actorsArray.map((actor) => {
-                    return (
-                        <li key={actor.id}  className={css.castItem}>
-                            <MovieCastItem picPath={actor.profile_path} character={actor.character} name={actor.name} />
-                        </li>  
-                    )
-                })}
-            </ul>
-                </div>
-        )
-    } else if (hasLoaded && actorsArray.length === 0) {
-        return (
-            <strong>Sorry, we don't have cast information for this movie at this time.</strong>
-        )
-    }
+
+    return (
+        <div>
+            {actorsArray.length > 0
+                && <div>
+                    <h2 className={css.castTitle}>The movie cast:</h2>
+                    <ul className={css.castList}>
+                        {actorsArray.map((actor) => {
+                            return (
+                                <li key={actor.id} className={css.castItem}>
+                                    <MovieCastItem picPath={actor.profile_path} character={actor.character} name={actor.name} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>}
+            {hasLoaded && actorsArray.length === 0 && <strong>Sorry, we don't have cast information for this movie at this time.</strong>}
+            {isError && <ErrorMessage/> }
+         </div>
+     )
 }
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -3,14 +3,15 @@ import MovieReviewsItem from '../MovieReviewsItem/MovieReviewsItem'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 export default function MovieReviews() {
     const { movieId } = useParams();
     const [reviewsArray, setReviewsArray] = useState([]);
     const [hasLoaded, setHasLoaded] = useState(false);
+    const [isError, setIsError] = useState(false);
     
     useEffect(() => {
-        setHasLoaded(false);
         const token = import.meta.env.VITE_TMDB_TOKEN;
         const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews`;
         const options = {
@@ -19,34 +20,35 @@ export default function MovieReviews() {
                 Authorization: `Bearer ${token}`,
             }
         };
+        setIsError(false);
+        setHasLoaded(false);
         axios.get(url, options)
             .then((res) => {
                 setReviewsArray(res.data.results);
                 setHasLoaded(true);
             })
             .catch(() => {
-                setReviewsArray([]);
-                setHasLoaded(true);
+                setIsError(true);
             });
     }, [movieId])
-    if (reviewsArray.length > 0) {
-        return (
-            <div className={css.container}>
-                <h2 className={css.reviewsTitle}>The movie reviews:</h2>
-                <ul className={css.reviewsList}>
-                    {reviewsArray.map((review) => {
-                        return (
-                            <li key={review.id} className={css.reviewsItem}>
-                                <MovieReviewsItem author={review.author} content={review.content} date={review.updated_at.substr(0, 10)} />
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    } else if (hasLoaded && reviewsArray.length === 0) {
-        return (
-            <strong>Sorry, we don't have reviws for this movie at this time.</strong>
-        )
-    }
+
+    return (
+        <div>
+            {reviewsArray.length > 0
+                && <div className={css.container}>
+                    <h2 className={css.reviewsTitle}>The movie reviews:</h2>
+                    <ul className={css.reviewsList}>
+                        {reviewsArray.map((review) => {
+                            return (
+                                <li key={review.id} className={css.reviewsItem}>
+                                    <MovieReviewsItem author={review.author} content={review.content} date={review.updated_at.substr(0, 10)} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>}
+            {hasLoaded && reviewsArray.length === 0 && <strong>Sorry, we don't have reviws for this movie at this time.</strong>}
+            {isError && <ErrorMessage/> }
+        </div>
+    )
 }
